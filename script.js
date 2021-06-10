8be7941937b14369c2a78aef3cbff3ade7e6f5e2
Rishabh Chawla <rishabh.chawla@sprinklr.com>
2021-06-10 14:56:11 +0530

    chore: restructure files

---
+++ script.js
@@ -0,0 +1,82 @@
+"use strict";
+
+
+const filesContainerNode = document.getElementsByClassName("files-container")[0];
+const previewImageNode = document.querySelector(".preview-container .preview");
+const previewTextNode = document.querySelector(".preview-container p");
+
+let files = [];
+let currentFileIndex = 0;
+
+const concatString = (str) => {
+    if (str.length <= 30) {
+        return str;
+    }
+
+    return str.substring(0, 17) + "..." + str.slice(-10);
+}
+
+const renderFiles = async () => {
+    for (const file in files) {
+        const fileNode = document.createElement("div");
+        
+        const thumbnailNode = document.createElement("div");
+        thumbnailNode.classList.add("thumbnail");
+        thumbnailNode.style.backgroundImage = `url("${files[file].previewImage}")`;
+        
+        const textContent = document.createTextNode(concatString(files[file].title));
+        
+        fileNode.classList.add("file");
+        fileNode.appendChild(thumbnailNode);
+        fileNode.appendChild(textContent);
+        fileNode.addEventListener("click", async () => {
+            setFileIndex(parseInt(file));
+            await renderPreview();
+        })
+        
+        filesContainerNode.appendChild(fileNode);
+    }
+    currentFileIndex = 0;
+}
+
+const renderPreview = async () => {
+    filesContainerNode.children[currentFileIndex].classList.add("focus");
+    previewImageNode.style.backgroundImage = `url("${files[currentFileIndex].previewImage}")`;
+    previewTextNode.innerHTML = files[currentFileIndex].title;
+}
+
+const fetchFiles = async () => {
+    const response = await fetch("/data.json");
+    files = await response.json();
+    console.log(files[0]);
+}
+
+const setFileIndex = (newIndex) => {
+    if (newIndex < 0 || newIndex >= files.length) return;
+    filesContainerNode.children[currentFileIndex].classList.remove("focus");
+    currentFileIndex = newIndex;
+}
+
+(async () => {
+    await fetchFiles();
+    await renderFiles();
+    await renderPreview();
+
+    // Register event listeners after initial render
+    document.body.addEventListener("keydown", async ({key}) => {
+        switch (key) {
+            case "ArrowUp":
+                setFileIndex(currentFileIndex - 1);
+                await renderPreview();
+                break;
+            
+            case "ArrowDown":
+                setFileIndex(currentFileIndex + 1);
+                await renderPreview();
+                break;
+
+            default:
+                break;
+        }
+    })
+})();
