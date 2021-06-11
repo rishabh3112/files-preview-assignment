0804268c055a9a8f946cbd4e449f1e560458e4e6
Rishabh Chawla <rishabh.chawla@sprinklr.com>
2021-06-11 10:49:42 +0530

    chore: format script

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 "use strict";
 
-
-const filesContainerNode = document.getElementsByClassName("files-container")[0];
+const filesContainerNode = document.querySelector(".files-container");
 const previewImageNode = document.querySelector(".preview-container .preview");
 const previewTextNode = document.querySelector(".preview-container p");
 
@@ -11,11 +10,11 @@ let currentFileIndex = 0;
 const renderFiles = async () => {
     for (const file in files) {
         const fileNode = document.createElement("div");
-        
+
         const thumbnailNode = document.createElement("div");
         thumbnailNode.classList.add("thumbnail");
         thumbnailNode.style.backgroundImage = `url("${files[file].previewImage}")`;
-        
+
         const textContent = document.createElement("span");
         textContent.innerText = files[file].title;
 
@@ -25,8 +24,8 @@ const renderFiles = async () => {
         fileNode.addEventListener("click", async () => {
             setFileIndex(parseInt(file));
             await renderPreview();
-        })
-        
+        });
+
         filesContainerNode.appendChild(fileNode);
     }
 
@@ -38,10 +37,14 @@ const renderFiles = async () => {
         const thumbnailStyle = window.getComputedStyle(thumbnailNode);
         const fileNameNode = fileNode.children[1];
         const textWidth = fileNameNode.clientWidth;
-        const boxWidth = fileNode.clientWidth - (thumbnailNode.clientWidth + parseInt(thumbnailStyle.marginLeft) + parseInt(thumbnailStyle.marginRight));
+        const boxWidth =
+            fileNode.clientWidth -
+            (thumbnailNode.clientWidth +
+                parseInt(thumbnailStyle.marginLeft) +
+                parseInt(thumbnailStyle.marginRight));
         // Width of text overflowed out of box
         const redundantWidth = textWidth - boxWidth;
-        
+
         if (redundantWidth <= 0) {
             continue;
         }
@@ -49,33 +52,39 @@ const renderFiles = async () => {
         // Truncate text if overflowing
         const textContent = fileNameNode.innerText;
         Object.assign(fileNameNode.style, {
-            "width": "50%",
-            "overflow": "hidden",
+            width: "50%",
+            overflow: "hidden",
             "word-break": "break-all",
             "white-space": "normal",
         });
-        fileNameNode.style.setProperty("--file-width", `${fileNameNode.clientWidth - 5}px`);
+        fileNameNode.style.setProperty(
+            "--file-width",
+            `${fileNameNode.clientWidth - 5}px`
+        );
         fileNameNode.setAttribute("data-before", textContent);
-        fileNameNode.style.setProperty("height", window.getComputedStyle(fileNameNode, ":before").height);
+        fileNameNode.style.setProperty(
+            "height",
+            window.getComputedStyle(fileNameNode, ":before").height
+        );
     }
-}
+};
 
 const renderPreview = async () => {
     filesContainerNode.children[currentFileIndex].classList.add("focus");
     previewImageNode.style.backgroundImage = `url("${files[currentFileIndex].previewImage}")`;
     previewTextNode.innerHTML = files[currentFileIndex].title;
-}
+};
 
 const fetchFiles = async () => {
     const response = await fetch("data.json");
     files = await response.json();
-}
+};
 
 const setFileIndex = (newIndex) => {
     if (newIndex < 0 || newIndex >= files.length) return;
     filesContainerNode.children[currentFileIndex].classList.remove("focus");
     currentFileIndex = newIndex;
-}
+};
 
 (async () => {
     await fetchFiles();
@@ -83,13 +92,13 @@ const setFileIndex = (newIndex) => {
     await renderPreview();
 
     // Register event listeners after initial render
-    document.body.addEventListener("keydown", async ({key}) => {
+    document.body.addEventListener("keydown", async ({ key }) => {
         switch (key) {
             case "ArrowUp":
                 setFileIndex(currentFileIndex - 1);
                 await renderPreview();
                 break;
-            
+
             case "ArrowDown":
                 setFileIndex(currentFileIndex + 1);
                 await renderPreview();
@@ -98,5 +107,5 @@ const setFileIndex = (newIndex) => {
             default:
                 break;
         }
-    })
+    });
 })();
